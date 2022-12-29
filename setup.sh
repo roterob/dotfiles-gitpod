f83847df9b8c9770aea5dc82aa91713c43d2b413
Ramón Otero Baltar <ramon.otero@factorial.co>
2022-12-29 13:06:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.ll log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
 # bash
 alias git_current_branch="git symbolic-ref --short HEAD"
